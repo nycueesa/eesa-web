1de51a6ef6bebb3e19fccafc6fd322197f1284ba
nycuecelightdance <nycuecelightdance@gmail.com>
2025-09-09 23:25:15 +0800

    feat:navbar, redux, module_css complete

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+import App from "./App.jsx";
+import { store, persistor } from "./redux/store.js"; // ✅ named import
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+        <App />
+    </PersistGate>
+  </Provider>
 );
---
+++ frontend/src/page/main/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Main.module.css"
+
+export default function Main(){
+
+    return(
+        <div className = {styles.mainPage}>
+            <h1> This is the main page. </h1>
+        </div>
+    )
+}
