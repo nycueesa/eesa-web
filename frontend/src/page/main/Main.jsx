8774ede360bbbb4fe775b2aa40639c935d3be6f2
Nick Chang <nick.ee12@nycu.edu.tw>
2025-09-23 21:14:32 +0800

    fix:add space

--- frontend/src/page/main/Main.jsx
+++ frontend/src/page/main/Main.jsx
@@ -3,6 +3,7 @@ import styles from "./Main.module.css"
 
 export default function Main(){
 
+    
     return(
         <div className = {styles.mainPage}>
             <h1> This is the main page. </h1>
