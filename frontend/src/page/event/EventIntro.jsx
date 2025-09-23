b574bb2477e8a48b0c0c519a2c6e6e0bd77b1280
Nick Chang <nick.ee12@nycu.edu.tw>
2025-09-23 21:05:34 +0800

    add: 增加活動頁照片

--- frontend/src/page/event/EventIntro.jsx
+++ frontend/src/page/event/EventIntro.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./EventIntro.module.css"
+import main_pic from "../../media/active-eecamp-1.jpg"
 
 export default function EventIntro(){
 
     return(
         <div className = {styles.eventIntroPage}>
+            <img src = {main_pic} className = {styles.main_pic}/>
             <h1> 活動簡介頁面 </h1>
         </div>
     )
