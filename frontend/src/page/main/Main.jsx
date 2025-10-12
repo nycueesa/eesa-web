0e57ea5ac5a87e6bcd940f56e0cbfbbdf59fa968
nanashi-484 <rick100054@gmail.com>
2025-10-12 22:01:05 +0800

    Revert "Nick"

--- frontend/src/page/main/Main.jsx
+++ frontend/src/page/main/Main.jsx
@@ -3,7 +3,6 @@ import styles from "./Main.module.css"
 
 export default function Main(){
 
-    
     return(
         <div className = {styles.mainPage}>
             <h1> This is the main page. </h1>
