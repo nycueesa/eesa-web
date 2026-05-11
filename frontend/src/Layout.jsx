df220eebde9b621768ebbc452168448274665043
rt_hsiao <rayhsiao0628@gmail.com>
2026-05-12 02:50:31 +0800

    fix: merge 小錯誤修正

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Link } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect, useState } from "react";
-import { useNavigate, useLocation } from 'react-router-dom';
-
-=======
-import React, { useState, useEffect } from 'react';
+﻿import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from "react-router-dom";
->>>>>>> 8399ea2cb6325191c4c6b3a28b02e701b9110dc9
 import { useSelector, useDispatch } from "react-redux";
 import { setPositionPage } from "./redux/commonSlice.js";
 import styles from "./Layout.module.css";
@@ -111,16 +98,7 @@ export default function Layout({ children }) {
         // 存 localStorage
         localStorage.setItem('lastPath', currentPath);
     }, [location.pathname, dispatch]);
-<<<<<<< HEAD
 
-    useEffect(() => {
-        console.log(positions);
-        if (positions !== location.pathname) {
-            navigate(positions, { replace: true });
-        }
-    }, [navigate, positions, location.pathname]);
-=======
->>>>>>> 8399ea2cb6325191c4c6b3a28b02e701b9110dc9
 
     return (
         <div>
